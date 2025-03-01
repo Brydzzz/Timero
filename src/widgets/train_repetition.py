a44fadb3aab2aaaf9665137daa7bc05917ce85a0
Brydzzz <mrufkazkrem@protonmail.com>
2025-03-01 15:45:36 +0100

    some styling still not there

--- src/widgets/train_repetition.py
+++ src/widgets/train_repetition.py
@@ -1,7 +1,9 @@
-from textual.containers import VerticalGroup
+from textual.containers import VerticalGroup, Container
 from textual.app import ComposeResult
-from textual.widgets import Label, Button
-from rich.text import Text
+from textual.widgets import Label, Button, Digits
+from art import text2art
+
+# from rich.text import Text
 from routine import RepetitionExercise
 
 
@@ -11,16 +13,40 @@ class TrainRepetitionWidget(VerticalGroup):
         TrainRepetitionWidget {
             background: $boost;
             align: center middle;
-            width: auto;
-            padding: 2 4;
+            max-width: 55%;
+            max-height: 45vh;
+            # padding: 1;
+            content-align: center middle;
         }
 
+
+        #exercise-name-container {
+            align: center middle;
+        }
 
         #rep-exercise-name {
             text-style: bold;
             border: ascii $border;
             padding: 0 2;
             text-align: center;
+        }
+
+
+        #repetitions-number {
+            text-align: center;
+            # text-style: bold;
+            width: 100%;
+        }
+
+        #repetitions-label {
+            text-align: center;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        #reps-finished {
+            align-horizontal: center;
         }
     """
 
@@ -35,15 +61,18 @@ class TrainRepetitionWidget(VerticalGroup):
     def change_exercise(self, exercise: RepetitionExercise) -> None:
         self.exercise = exercise
         self.query_one("#rep-exercise-name", Label).update(self.exercise.name)
-        self.query_one("#repetitions-count-label", Label).update(
-            Text(f"Repetitions: {self.exercise.repetitions}")
+        self.query_one("#repetitions-number", Digits).update(
+            f"{self.exercise.repetitions}"
         )
 
     def compose(self) -> ComposeResult:
-        yield Label(self.exercise.name, id="rep-exercise-name")
+        with Container(id="exercise-name-container"):
+            yield Label(self.exercise.name, id="rep-exercise-name")
+        # with HorizontalGroup(id="repetitions-container"):
+        yield Digits(f"{self.exercise.repetitions}", id="repetitions-number")
         yield Label(
-            Text(f"Repetitions: {self.exercise.repetitions}"),
-            id="repetitions-count-label",
+            text2art("REPETITIONS", "straight"),
+            id="repetitions-label",
         )
         yield Button("Finished", id="reps-finished", variant="success")
 
