6b25493f092400ba825610e0ddaa3b8ba0288aba
Brydzzz <mrufkazkrem@protonmail.com>
2025-03-03 11:18:55 +0100

    added breaks between exercises

--- src/screens/train_view.py
+++ src/screens/train_view.py
@@ -1,3 +1,4 @@
+from textual import on
 from textual.screen import Screen
 from textual.app import ComposeResult
 from textual.widgets import Header, Footer, Button
@@ -15,19 +16,38 @@ class TrainView(Screen):
         super().__init__(*args, **kwargs)
         self.routine = self.app.routines[self.app.curr_routine_idx]
         self.exercise_iter = iter(self.routine.exercises)
+        self.total_exercises = len(self.routine.exercises)
+        self.completed_exercises = 0
 
-    def handle_next_exercise(self):
-        e = next(self.exercise_iter, None)
-        if not e:
-            self.app.screen_manager.go_to_routine()
-            return
-
+    def _remove_train_widgets(self) -> None:
         children = self.query_children()
         train_widgets = children.exclude(".no-remove")
         self.remove_children(train_widgets)
 
+    def start_break_timer(self) -> None:
+        if self.total_exercises == self.completed_exercises:
+            self.app.screen_manager.go_to_routine()
+            # TODO: switch to end screen or notify user?
+            return
+        self._remove_train_widgets()
+        self.break_timer.reset_timer()
+        self.break_timer.remove_class("hide")
+
+    def handle_next_exercise(self):
+        self._remove_train_widgets()
+        e = next(self.exercise_iter, None)
+        if not e:
+            self.app.screen_manager.go_to_routine()
+            # TODO: switch to end screen or notify user?
+            return
+
+        self.completed_exercises += 1
         if isinstance(e, DurationExercise):
-            timer_widget = Timer(title=e.name, duration_time=e.duration)
+            timer_widget = Timer(
+                title=e.name,
+                duration_time=e.duration,
+                classes="exercise-timer",
+            )
             self.mount(timer_widget)
         elif isinstance(e, RepetitionExercise):
             train_widget = TrainRepetitionWidget(e)
@@ -35,20 +55,36 @@ class TrainView(Screen):
 
     def compose(self) -> ComposeResult:
         yield Header(classes="no-remove")
+        BREAK_DURATION = 7  # TODO: in future value from user settings
+        self.break_timer = Timer(
+            title="Break",
+            duration_time=BREAK_DURATION,
+            id="break-timer",
+            classes="no-remove hide",
+        )
+        yield self.break_timer
         yield Footer(classes="no-remove")
 
     def on_mount(self) -> None:
         self.handle_next_exercise()
 
-    def on_time_display_ended(self, event: TimeDisplay.Ended) -> None:
+    @on(TimeDisplay.Ended, ".exercise-timer TimeDisplay")
+    def exercise_timer_ended(self) -> None:
+        self.start_break_timer()
+
+    @on(TimeDisplay.Ended, "#break-timer TimeDisplay")
+    def break_timer_ended(self) -> None:
+        self.break_timer.add_class("hide")
         self.handle_next_exercise()
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         button_id = event.button.id
         if button_id == "reps-finished":
-            self.handle_next_exercise()
-        elif button_id == "skip-exercise":
-            self.handle_next_exercise()
+            self.start_break_timer()
 
     def action_skip_exercise(self):
-        self.handle_next_exercise()
+        if self.break_timer.has_class("hide"):
+            self.start_break_timer()
+        else:
+            self.break_timer.add_class("hide")
+            self.handle_next_exercise()
